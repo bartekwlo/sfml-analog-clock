cc5205cb1b4d75550751a36eae62f10e4d3ae8cc
Bartlomiej Wlodarczak <bartekwlo@gmail.com>
2018-03-26 00:29:46 +0200

    Added comments in clock-window.cpp

--- src/clock-window.cpp
+++ src/clock-window.cpp
@@ -1,14 +1,18 @@
 #include "clock-window.hpp"
 
+
+// ClockWindow class constructor
 ClockWindow::ClockWindow(int width, int height) : mWidth(width), mHeight(height) {
     mClock = new Clock();
     mWindow.create(sf::VideoMode(mWidth, mHeight), "SFML Analog Clock");
 }
 
+// ClockWindow class destructor
 ClockWindow::~ClockWindow() {
     delete mClock;
 }
 
+// ClockWindow main event loop method
 int ClockWindow::mainLoop() {
     while (mWindow.isOpen()) {
         sf::Event event;
@@ -24,6 +28,7 @@ int ClockWindow::mainLoop() {
     return 0;
 }
 
+// Returns pointer to the Clock object
 Clock* ClockWindow::getClock() {
     return mClock;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -11,5 +11,5 @@ int main(int argc, char *argv[]) {
     int ret;
     ret = cwin.mainLoop();
 
-    return ret;;
+    return ret;
 }
