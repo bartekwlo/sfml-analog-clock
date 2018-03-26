719d1a16e53bee223b76e1643211f70d47be16ee
Bartlomiej Wlodarczak <bartekwlo@gmail.com>
2018-03-26 23:33:18 +0200

    Deleted redundant constructor in ClockHand classs

--- src/clock-hand.hpp
+++ src/clock-hand.hpp
@@ -3,14 +3,9 @@
 
 #include <SFML/Graphics.hpp>
 
-#define DEFAULT_CLOCK_HAND_SIZE sf::Vector2f(3, 240)
-#define DEFAULT_CLOCK_HAND_COLOR sf::Color::Black
-#define DEFAULT_CLOCK_HAND_POSITION sf::Vector2f(400, 300)
-
 class ClockHand : public sf::RectangleShape {
     private:
     public:
-        ClockHand();
         ClockHand(sf::Vector2f position, sf::Vector2f size, sf::Color color);
         void configure(sf::Vector2f position, sf::Vector2f size, sf::Color color);
 };
